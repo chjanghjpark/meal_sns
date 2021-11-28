bc09944b260baab8bb32d8577e241bc420d08849
rtf400 <rtf400@naver.com>
2021-11-28 21:54:23 +0900

    * logout 기능 추가

--- src/map/sub/ProfileButton.jsx
+++ src/map/sub/ProfileButton.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button'
+import { OverlayTrigger, Popover, ListGroup } from "react-bootstrap";
 import jwt_decode from "jwt-decode";
 import ConvertNameToRGB from '../../utils/utils';
 
@@ -22,22 +23,55 @@ const ProfileButton = () => {
     }
   }, []);
 
+  const onClickMypage = useCallback(() => {
+    alert('아직 그런거 없다..');
+  }, []);
+
+  const onClickLogout = useCallback(() => {
+    localStorage.removeItem('share-meal-token');
+    setUserName('');
+    setUserID('');
+  }, []);
+
   return (
     <>
       {
         userName === '' || userID === ''
           ?
           <Button variant="outline-success" href="./login">Login</Button>
-          : <div style={{
-            width: "38px",
-            height: "38px",
-            lineHeight: "38px",
-            borderRadius: "50%",
-            fontSize: "15px",
-            color: "#fff",
-            textAlign: "center",
-            background: ConvertNameToRGB(userID),
-          }}>{userName[0]}</div>
+          :
+          <OverlayTrigger
+            placement="bottom"
+            trigger="click"
+            overlay={
+              <Popover id="popover-basic">
+                <Popover.Header as="h3">{userName} 님</Popover.Header>
+                <ListGroup>
+                  <ListGroup.Item action onClick={onClickMypage}>My page</ListGroup.Item>
+                  <ListGroup.Item action onClick={onClickLogout}>Logout</ListGroup.Item>
+                </ListGroup>
+              </Popover>
+            }
+          >
+            {({ ref, ...triggerHandler }) => (
+              <div
+                style={{
+                  width: "38px",
+                  height: "38px",
+                  lineHeight: "38px",
+                  borderRadius: "50%",
+                  fontSize: "15px",
+                  color: "#fff",
+                  textAlign: "center",
+                  background: ConvertNameToRGB(userID),
+                }}
+                ref={ref}
+                {...triggerHandler}
+              >
+                {userName[0]}
+              </div>
+            )}
+          </OverlayTrigger>
       }
     </>
   );
